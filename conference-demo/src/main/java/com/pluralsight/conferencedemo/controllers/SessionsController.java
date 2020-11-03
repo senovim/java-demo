e2e72d78619b664515da893ec9226c8896d3ab0a
Jelena Cupac <jelena.cupac@optivem.com>
2020-11-03 21:54:59 +0100

    create, update, delete functionality for CRUD rest endpoints

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -1,7 +1,10 @@
+// session JPA repository
+
 package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -28,11 +31,30 @@ public class SessionsController {
         return sessionRepository.getOne(id);
     }
 
-//    @PostMapping
-//    // so that it maps 201, not 200
-//    // @ResponseStatus(HttpStatus.CREATED)
-//    public Session create(@RequestBody final Session session) {
-//        return sessionRepository.saveAndFlush(session);
-//    }
+    @PostMapping
+    // so that it maps 201, not 200
+    // @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    // delete endpoint
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // Also need to check for children records before deleting.
+        sessionRepository.deleteById(id);
+        // above will only delete session repositories, research how to delete children records
+    }
+
+    // updating session record
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what
+        // for PUT - if attribute not specified, it will eb set to null
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
 
 }
