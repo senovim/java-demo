974739e5d9767bf233d62e6ffabf2cb8b35870ab
Jelena Cupac <jelena.cupac@optivem.com>
2020-11-05 10:11:42 +0100

    create configuration in java code (working)
    usernaem and password added

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -12,8 +12,24 @@ public class PersistenceConfiguration {
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        // password field declaration not necessary if empty i.e. null
+        builder.username("postgres");
+        builder.password("admin");
         // visible in the log startup, when we run it look in the log below
         System.out.println("My custom datasource has been initialized and set");
         return builder.build();
     }
 }
+
+/*
+@Bean
+public DataSource dataSource() {
+    BasicDataSource dataSource = new BasicDataSource();
+    dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+    dataSource.setUrl(env.getProperty("jdbc.url"));
+    dataSource.setUsername(env.getProperty("jdbc.user"));
+    dataSource.setPassword(env.getProperty("jdbc.password"));
+
+    return dataSource;
+}
+ */
