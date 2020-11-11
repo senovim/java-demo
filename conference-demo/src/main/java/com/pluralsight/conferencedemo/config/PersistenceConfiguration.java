3e0ac3ab29bc1ed26904ee88d495710c9f860c6f
Jelena Cupac <jelena.cupac@optivem.com>
2020-11-11 21:51:49 +0100

    WAR deployment - Tomcat Server

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,17 +8,17 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-//    @Bean
-//    public DataSource dataSource() {
-//        DataSourceBuilder builder = DataSourceBuilder.create();
-//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-//        // password field declaration not necessary if empty i.e. null
-//        builder.username("postgres");
-//        builder.password("admin");
-//        // visible in the log startup, when we run it look in the log below
-//        System.out.println("My custom datasource has been initialized and set");
-//        return builder.build();
-//    }
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        // password field declaration not necessary if empty i.e. null
+        builder.username("postgres");
+        builder.password("admin");
+        // visible in the log startup, when we run it look in the log below
+        System.out.println("My custom datasource has been initialized and set");
+        return builder.build();
+    }
 }
 
 /*
